2c192b7fe6373bbd3b7c0ea2734d32a205a066b6
HaloT455 <halot652@gmail.com>
2026-08-23 13:02:39 +0700

    Update sys.c
    Signed-off-by: HaloT455 <halot652@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1354,7 +1354,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.3020");
+			strcpy(tmp.release, "5.10.250");
 			pr_debug("fake uname: %s/%d release=%s\n",
 				 current->comm, current->pid, tmp.release);
 		}
