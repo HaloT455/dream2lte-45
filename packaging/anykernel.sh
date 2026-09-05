f1da0367fea1ebb241e952ad1119e023ceba1308
HaloT455 <halot652@gmail.com>
2026-09-06 00:47:46 +0700

    packaging: label the Full MGLRU SimpleLMK candidate

--- packaging/anykernel.sh
+++ packaging/anykernel.sh
@@ -1,7 +1,7 @@
 ### AnyKernel3 Ramdisk Mod Script
 
 properties() { '
-kernel.string=ALice V8 Rescue SimpleLMK Universal
+kernel.string=ALice V8 Full MGLRU SimpleLMK SuspendFix
 do.devicecheck=1
 do.modules=0
 do.systemless=1
